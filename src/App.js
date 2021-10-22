84cb057896a21615adc84d7775576f202c425d1e
Balazs Borsi <bborsi@emarsys.com>
2021-10-22 22:53:07 +0200

    doctor page component renamed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import {Router, Route, Switch} from "react-router-dom";
 import DoctorPage from "./pages/DoctorPage";
@@ -9,20 +8,6 @@ function App() {
   return (
     <div className="App">
       <CustomNavbar />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <Router history={history}>
         <Switch>
           <Route exact={true} path={['/doctor']} component={DoctorPage}/>
