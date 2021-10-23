58edfa81efdf690bbf89b14dafa82a7c58abc00d
dszendrei <r.daniel.szendrei@gmail.com>
2021-10-23 17:35:37 +0200

    Get image from painting

--- src/pages/PatientStatusReportPage.js
+++ src/pages/PatientStatusReportPage.js
@@ -10,8 +10,26 @@ function PatientStatusReportPage() {
         history.push(route);
     }
 
+    let image;
+
+    const getImage = () => {
+        setTimeout(() => {
+            fetch('https://junctionx2021dd.herokuapp.com/getpaint')
+            .then(response => response.text())
+            .then(data => {
+                image = data;
+                console.log('Success on loading:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        }, 2000)
+    }
+
+    getImage();
+
     return (
-        <div className='patient-status-report-page'>
+        <div className='patient-status-report-page' onLoad={() => {getImage()}}>
             <h1>Please fill the form</h1>
             <Button variant='primary' onClick={() => {
                 clickHandler('/submitreport');
