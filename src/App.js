ef664d22a69aa4c9d5cc98b5dc8636bac4592d77
dszendrei <r.daniel.szendrei@gmail.com>
2021-10-23 11:26:41 +0200

    Add form page for status report

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Switch, useHistory} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import DoctorPage from "./pages/DoctorPage";
 import PatientPage from "./pages/PatientPage";
 import PatientStatusReportPage from "./pages/PatientStatusReportPage";
+import PatientStatusReportPainMarkPage from "./pages/PatientStatusReportPainMarkPage";
 import CustomNavbar from "./components/CustomNavbar";
 import Home from "./pages/Home";
 import React from "react";
@@ -18,7 +19,9 @@ function App() {
                     <Route exact path={['/doctor']} component={DoctorPage}/>
                     <Route exact path={['/patient']} component={PatientPage}/>
                     <Route exact path={['', '/', '/home']} component={Home}/>
-                    <Route exact path={['/createreport']} component={PatientStatusReportPage}/>
+                    <Route exact path={['/createreport']} component={PatientStatusReportPainMarkPage}/>
+                    <Route exact path={['/continuereport']} component={PatientStatusReportPage}/>
+                    <Route exact path={['/submitreport']} component={PatientPage}/>
                     <Route exact path={['/viewreports']} component={PatientStatusReportPage}/>
                 </Switch>
             </Router>
