3fb37f9b25abcc882cf1c61c352128e369f6b271
Adam Bozsoki <adam.bozsoki@emarsys.com>
2021-10-24 03:02:56 +0200

    Add messages functionality, now it is only working with localhost

--- src/components/MessagesComponent/MessagesComponent.js
+++ src/components/MessagesComponent/MessagesComponent.js
@@ -1,11 +1,47 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import './MessagesComponent.css';
-import MessageComponent from "../MessageComponent/MessageComponent";
-import MyMessageComponent from "../MessageComponent/MyMessageComponent";
+import {Button, Card, Form} from "react-bootstrap";
 
 const MessagesComponent = (props) => {
   const {patientList} = props;
-  const [selectedPatientForMessage, setSelectedPatientForMessage] = useState(null)
+  const [selectedPatientForMessage, setSelectedPatientForMessage] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [text, setText] = useState("");
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+      console.log('Rohadek messagek', messages);
+  })
+
+    useEffect(() => {
+        let serverUrl = "ws://localhost:8080/";
+        let webSocket = new WebSocket(serverUrl + 'chat');
+
+        webSocket.onmessage = (message) => {
+            let parsedData = JSON.parse(message.data);
+            parsedData.sender = "Doctor Strange";
+            console.log(parsedData);
+            // addMessage(parsedData);
+            messages.push(parsedData);
+            setMessages([...messages]);
+        };
+        setSocket(webSocket);
+        console.log("successfully connected to socket");
+    }, []);
+
+
+  const addMessage = (message) => {
+      const newMessages = [...messages, message];
+      setMessages(newMessages);
+  }
+
+  const sendMessage = () => {
+      socket.send(JSON.stringify({
+          "message": text,
+          "sentByDoctor": true
+      }));
+      setText('');
+  }
 
   return (
     <div id='messages-container'>
@@ -26,8 +62,76 @@ const MessagesComponent = (props) => {
         {selectedPatientForMessage ? (
           <div>
             <h3>{selectedPatientForMessage.name}</h3>
-            <MessageComponent message={"mockedMessage"}></MessageComponent>
-            <MyMessageComponent message={"mockedMyMessage"}></MyMessageComponent>
+              <div id="messageContainer">
+                {messages.map((message, key) => {
+                    if (message.sentByDoctor) {
+                        return (
+                            <div key={key} className="card-container left">
+                                <Card
+                                    bg='dark'
+                                    text='light'
+                                    className="mb-2 flex-end"
+                                    style={{ width: '18rem' }}
+                                >
+                                    <Card.Header>
+                                        {message.sender}
+                                    </Card.Header>
+                                    <Card.Text>
+                                        {message.message}
+                                    </Card.Text>
+                                </Card>
+                            </div>
+                        )
+                    }
+                     return (
+                         <div key={key} className="card-container right">
+                             <Card
+                                 bg='light'
+                                 text='dark'
+                                 className="mb-2 flex-start"
+                                 style={{ width: '18rem' }}
+                             >
+                                 <Card.Header>
+                                     {message.sender}
+                                 </Card.Header>
+                                 <Card.Text>
+                                     {message.message}
+                                 </Card.Text>
+                             </Card>
+                         </div>
+                     )
+                    }
+                )}
+              </div>
+
+
+              <div className="fixed-bottom container">
+                  <div className="center-align">
+                      <div className="row">
+                          <Form>
+                              <Form.Control
+                                  id="chatInput"
+                                  onChange={
+                                      (event => setText(event.target.value))
+                                  }
+                                  value={text}
+                              />
+                                  <Button
+                                      id="sendChat"
+                                      onClick={(event) => {
+                                        sendMessage();
+                                      }}
+                                  > kattanj rá</Button>
+                          </Form>
+                      </div>
+
+                  </div>
+
+              </div>
+
+
+            {/*<MessageComponent message={"mockedMessage"}></MessageComponent>
+            <MyMessageComponent message={"mockedMyMessage"}></MyMessageComponent>*/}
           </div>
         ) : (
           <div>Please select Patient to show messages.</div>
